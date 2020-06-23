cb1f1ed9322ba3d67e17983c982a75f95e81cb37
paperjar <dreamtnem@gmail.com>
2020-06-23 14:59:21 +0200

    Updates

---
+++ precache-manifest.2bb939cee07e47a66c2b4615c7455153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b415969dab0f16b4e7530205fada45",
+    "url": "/country-api/index.html"
+  },
+  {
+    "revision": "1b77b586f1df5f0f6660",
+    "url": "/country-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0570ec82ff7db3a6c96e",
+    "url": "/country-api/static/css/main.f720ebc9.chunk.css"
+  },
+  {
+    "revision": "1b77b586f1df5f0f6660",
+    "url": "/country-api/static/js/2.1e6bf7cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-api/static/js/2.1e6bf7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0570ec82ff7db3a6c96e",
+    "url": "/country-api/static/js/main.fa3fa458.chunk.js"
+  },
+  {
+    "revision": "c19b1824c89858be90dd",
+    "url": "/country-api/static/js/runtime-main.11c89d15.js"
+  }
+]);
